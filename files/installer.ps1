2a112fce75c2f3d0b953ca7a139071cdb194869e
tobiasasa <tobias.asa@hotmail.com>
2022-09-18 17:19:18 -0700

    fe

--- files/installer.ps1
+++ files/installer.ps1
@@ -5,11 +5,10 @@ function random_text {
 	return -join ((65..90) + (97..122) | Get-Random -Count 5 | % {[char]$_})
 }
 
-
 #Variables 
 $wd = random_text
 $path = "$env:temp/$wd"
-echo $path
+
 
 #Crear directorio
 mkdir $path
