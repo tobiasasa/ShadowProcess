2588e8199853a9dbf4a9aee732b315711f763ded
tobiasasa <tobias.asa@hotmail.com>
2022-09-20 15:32:59 -0700

    a

--- files/installer.ps1
+++ files/installer.ps1
@@ -8,7 +8,7 @@ function random_text {
 #Variables 
 $wd = random_text
 $path = "$env:temp/$wd"
-$initial_dir = %cd%
+$initial_dir = "%cd%"
 
 #ir al directorio temporal y hacer directorio funcional
 mkdir $path
